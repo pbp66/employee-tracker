142e1134d036e1ebd97e546fb9b756047bd6a6ba
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-18 22:32:19 -0500

    Implemented add Department

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import Menu from "./lib/menu.js";
 
+import Company from "./lib/company/company.js";
+
 function init() {
 
 }
@@ -11,3 +13,10 @@ function main() {
 init();
 
 main();
+
+// DEV Testing Section
+const test = new Company();
+//const result = await test.addDepartment("Test");
+const result = await test.getAllDepartments();
+console.log("Get All Departments Response:")
+console.log(result);
