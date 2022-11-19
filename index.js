468bf7c9942de776f51efdc09eb54a369711c59f
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-19 00:17:17 -0500

    Implemented addEmployee()

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import Menu from "./lib/menu.js";
-
 import Company from "./lib/company/company.js";
 
 function init() {
@@ -17,5 +16,7 @@ main();
 // DEV Testing Section
 const test = new Company();
 //const result = await test.addDepartment("Test");
-const result = await test.addRole('Accounting Admin', 60000, 'Accounting');
-console.log(result);
+const result1 = await test.addEmployee('Abraham Lincoln', 'Operations Manager', null);
+const result2 = await test.addEmployee('Adam Seist', 'Production Engineer', 'Abraham Lincoln');
+console.log(result1);
+console.log(result2);
